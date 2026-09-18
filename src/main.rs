f22999ca9a28ed5dcfa555e6f8ba3161a224d016
Dmitriy Barabanov <qbarabanov@gmail.com>
2026-09-19 01:03:31 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    std::io::stdin().read_to_string(&mut input).expect("Error!");
+
+    check(&input);
+}
+
+fn check(input: &str) {
+    let mut parts = input.split_whitespace();
+
+    let first_number: i128 = parts
+        .next()
+        .expect("Empty!")
+        .parse()
+        .expect("Not a Number!");
+
+    let second_number: i128 = parts
+        .next()
+        .expect("Empty!")
+        .parse()
+        .expect("Not a Number!");
+
+    let sum: i128 = first_number + second_number;
+
+    println!("{}", sum);
 }
