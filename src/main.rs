57066cd76c3d969ed1fa084279a637c1bfec8d25
Dmitriy Barabanov <qbarabanov@gmail.com>
2026-09-19 12:55:27 +0300

    2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, Read};
 
 fn main() {
     let mut input = String::new();
-    
+
     io::stdin().read_to_string(&mut input).expect("Error!");
 
     let mut parts = input.split_whitespace();
