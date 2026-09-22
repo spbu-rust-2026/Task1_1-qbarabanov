dde5a30153c1674842b6a05d9fc9f875bc3781bb
Dmitriy Barabanov <qbarabanov@gmail.com>
2026-09-22 20:01:09 +0300

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_to_string(&mut input).expect("Error!");
+
+    let mut parts = input.split_whitespace();
+
+    let first_number: i128 = parts
+        .next()
+        .expect("Empty!")
+        .parse()
+        .expect("Not a number!");
+
+    let second_number: i128 = parts
+        .next()
+        .expect("Empty!")
+        .parse()
+        .expect("Not a number!");
+
+    println!("{}", first_number + second_number);
 }
